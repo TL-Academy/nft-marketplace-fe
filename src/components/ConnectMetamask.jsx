4616faf0c5ab8b6719fc39135a80a16042c5fdc7
Darin Ivanov <d.v.ivanov98@gmail.com>
2023-11-02 11:08:40 +0200

    implement web3 onboard

--- src/components/ConnectMetamask.jsx
+++ src/components/ConnectMetamask.jsx
@@ -1,23 +1,50 @@
-import { useEffect, useState } from "react";
-import { ethers } from 'ethers';
+import { init, useConnectWallet } from '@web3-onboard/react'
+import injectedModule from '@web3-onboard/injected-wallets'
+import { useEffect } from 'react'
+
+const rpcUrl = "https://eth-sepolia.g.alchemy.com/v2/EB_Y3YZKU0iLP46kusxjohflv9CKjsbH"
+
+const injected = injectedModule();
+
+init({
+    connect: {
+        autoConnectLastWallet: true
+    },
+    wallets: [injected],
+    chains: [{
+        id: "0xaa36a7",
+        token: "ETH",
+        label: "Ethereum Sepolia",
+        rpcUrl,
+    }]
+})
 
 export default function ConnectMetamask() {
 
-    const [currentAccount, setCurrentAccount] = useState(null);
+    const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
 
     useEffect(() => {
         if (!window.ethereum) {
-            alert("install MetaMask")
-        } else {
-            new ethers.providers.Web3Provider(window.ethereum)
-                .send("eth_requestAccounts", [])
-                .then((accounts) => {
-                    // when store set up, dispatch to redux instead of useState
-                    if (accounts.length > 0) setCurrentAccount(accounts[0]);
-                })
-                .catch((e) => console.log(e));
+            alert("install MetaMask");
         }
     }, [])
 
-    return
+    return (
+        <button
+            className="bg-orange-200 w-fit mt-2 rounded px-4 py-1 border border-orange-500 transform hover:scale-105 hover:bg-orange-300"
+            disabled={connecting}
+            onClick={() => { wallet ? disconnect(wallet) : connect() }}
+        >
+            {connecting ? "connecting" : wallet ? "disconnect" : "connect"}
+        </button>
+    )
+
 }
+
+// import { useWallets } from "@web3-onboard/react"
+// const connectedWallets = useWallets();
+// const injectedProvider = connectedWallets[0].provider;
+// const provider = new ethers.providers.Web3Provider(injectedProvider);
+// const signer = provider.getSigner();
+// const contract = new ethers.Contract(ADRESS, ABI, signer);
+// contract.executeWhatever();
