13e301ccdf3cf9dee90ae5a9cd0772c5c25c1003
VladichaYT <vladistefanov83@gmail.com>
2023-11-07 11:09:02 +0200

    Resolve conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css';
 import MintForm from './components/MintForm/MintForm';
 import NavBar from './components/NavBar/NavBar';
+import ConnectMetamask from './components/ConnectMetamask';
+import NFTCard from './components/NFTCard/NFTCard';
 import { Route, Routes } from 'react-router-dom'
+
 function App() {
     return (
         <>
@@ -9,10 +12,10 @@ function App() {
             <main className='mx-auto w-full max-w-[2560px] px-4 sm:px-8 xxl:px-16'>
                 <Routes>
                     <Route path='/create' element={<MintForm />} />
+                    <Route path='/login' element={<ConnectMetamask />} />
+                    <Route path='/card-info' element={<NFTCard />} />
                 </Routes>
             </main>
         </>
     );
 }
-
-export default App;
