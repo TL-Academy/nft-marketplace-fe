7f72d05edb3c0303525bf585fe42d72523973932
Chavdar <ch.tonchev94@gmail.com>
2023-11-07 21:33:57 +0200

    Fixed App.jsx bugs

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,21 @@ import MintForm from './components/MintForm/MintForm';
 import NavBar from './components/NavBar/NavBar';
 import ConnectMetamask from './components/ConnectMetamask';
 import NFTCard from './components/NFTCard/NFTCard';
-import { Route, Routes } from 'react-router-dom'
-
-// 
-import ConnectMetamask from './components/ConnectMetamask';
-import NFTCard from './components/NFTCard/NFTCard';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
     return (
         <>
             <NavBar />
-            <main className='mx-auto w-full max-w-[2560px] px-4 sm:px-8 xxl:px-16'>
+            <main className="mx-auto w-full max-w-[2560px] px-4 sm:px-8 xxl:px-16">
                 <Routes>
-                    <Route path='/create' element={<MintForm />} />
-                    <Route path='/login' element={<ConnectMetamask />} />
-                    <Route path='/card-info' element={<NFTCard />} />
+                    <Route path="/create" element={<MintForm />} />
+                    <Route path="/login" element={<ConnectMetamask />} />
+                    <Route path="/card-info" element={<NFTCard />} />
                 </Routes>
             </main>
         </>
     );
 }
+
+export default App;
