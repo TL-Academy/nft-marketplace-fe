d2656759faa1ecffd60a8d3b44b60e0eba8cfeab
Niki5225 <122698681+Niki5225@users.noreply.github.com>
2023-12-13 19:45:31 +0200

    Update reducer.js

--- src/app/reducer.js
+++ src/app/reducer.js
@@ -1,8 +1,10 @@
-import { combineReducers } from "redux";
+import {combineReducers} from "redux";
 import notificationReducer from './notification.js';
 import themeReducer from "../components/ThemeSwitcher/themeSlice.js";
+import walletReducer from "../components/walletReducer.js";
 
 export default combineReducers({
     notification: notificationReducer,
     theme: themeReducer,
+    wallet: walletReducer
 })
