38ab53d26db0c2a25b242c36a28d9b0cb3a8b7d0
VladichaYT <vladistefanov83@gmail.com>
2023-12-25 12:11:31 +0200

    Add owner and contract address to minted Nfts Redux

--- src/utils/ethers/ethers.js
+++ src/utils/ethers/ethers.js
@@ -9,24 +9,22 @@ import { addMintedNFT, setMintedNFTs } from '../../redux/collectionSlice.js';
 import { store } from '../../redux/store.js';
 import addresses from '../../contracts/addresses.json';
 
+const addressesKey = Object.keys(addresses)[0];
+const fromBlock = addresses[addressesKey].NftCollections.BoringTokenNFT.fromBlock;
+
 export const provider = new ethers.providers.JsonRpcProvider(PROVIDER_ADDRESS);
 
 const abi = await fetch(
-    `https://api-sepolia.etherscan.io/api?module=contract&action=getabi&address=${CONTRACT_ADDRESS}&apikey=${ETHERSCAN_API_KEY}}`,
-)
+    `https://api-sepolia.etherscan.io/api?module=contract&action=getabi&address=${CONTRACT_ADDRESS}&apikey=${ETHERSCAN_API_KEY}}`,)
     .then((res) => res.json())
     .then((res) => res.result)
     .catch((e) => console.error('Error getting the ABI from Etherscan: ', e));
 
 const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, provider);
 
-const addressesKey = Object.keys(addresses)[0];
-const fromBlock = addresses[addressesKey].NftCollections.BoringTokenNFT.fromBlock;
-
 export const getAllMintedNFTs = () => {
     return async function (dispatch) {
         try {
-
             const filter = contract.filters[EventTypes.MINTED]();
             const nfts = await contract.queryFilter(filter, fromBlock, 'latest');
 
@@ -36,7 +34,10 @@ export const getAllMintedNFTs = () => {
                     const data = await res.json();
 
                     data.image = data.image.replace(/^ipfs:\/\//, '');
-                    return data;
+                    data.address = nft.address
+                    data.owner = nft.args.to
+
+                    return data
                 }),
             );
 
@@ -53,11 +54,10 @@ export const getUserNfts = async () => {
             const filter = contract.filters[EventTypes.MINTED]();
             const nfts = await contract.queryFilter(filter, fromBlock, 'latest');
 
-            console.log(nfts);
             const userNfts = await Promise.all(
                 nfts.map(async (nft) => {
                     //get userId from connectMetamask (look at NavBar component)
-                    if(nft.args[1] == userId){
+                    if(nft.args.to == userId){
                         const res = await fetch(`https://ipfs.io/ipfs/${nft.args.tokenHash}`);
                         const data = await res.json();
 
