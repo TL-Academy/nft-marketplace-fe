6c6cc44d5250dc6d44b9f05106a30bf9ae1f5ada
Chavdar <chokobg@gmail.com>
2024-01-16 17:56:56 +0200

    add price modal

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import styles from './Modal.module.css';
+
+const Backdrop = (props) => {
+    const backDropOnClick = () => {
+        props.onClose();
+    };
+    return <div className={styles['backdrop']} onClick={backDropOnClick} />;
+};
+
+const ModalOverlay = (props) => {
+    return (
+        <div className={styles['modal']}>
+            <div className={styles['content']}>{props.children}</div>
+        </div>
+    );
+};
+
+const portalOverlay = document.getElementById('overlays');
+
+const Modal = (props) => {
+    return (
+        <React.Fragment>
+            {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalOverlay)}
+            {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalOverlay)}
+        </React.Fragment>
+    );
+};
+
+export default Modal;
