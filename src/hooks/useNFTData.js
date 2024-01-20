0559bce8fb0134343236041b8dc73b009351f77e
Chavdar <chokobg@gmail.com>
2024-01-20 12:50:53 +0200

    fix loading nfts

--- src/hooks/useNFTData.js
+++ src/hooks/useNFTData.js
@@ -3,11 +3,21 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { selectListedNFTs } from '../redux/getListedNFTS';
 import { getMintedNFTs, filterListedNFTs } from '../redux/collectionSlice';
+import { getListedNFTs, getAllMintedNFTs } from '../utils/ethers/ethers';
 
 const useNFTData = () => {
     const dispatch = useDispatch();
     const nfts = useSelector(getMintedNFTs);
     const listedNFTs = useSelector(selectListedNFTs);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await dispatch(getAllMintedNFTs());
+            await dispatch(getListedNFTs());
+        };
+
+        fetchData();
+    }, [dispatch]);
 
     useEffect(() => {
         if (nfts !== undefined) {
