fbce8de886904d0a498c99d877a1f700248bd383
darinvi <d.v.ivanov98@gmail.com>
2024-01-29 13:26:13 +0200

    buy works

--- src/services/buyNFT.js
+++ src/services/buyNFT.js
@@ -1,13 +1,14 @@
 import getContract from '../utils/ethers/getContract';
 import addresses from '../contracts/addresses.json';
+import { ethers } from 'ethers';
 
 export default async function buyNFT(address, cardId, price) {
     const maketplaceAddress = addresses[11155111].Marketplace.address;
     const contract = await getContract(maketplaceAddress);
-    console.log(price)
+    const options = {value: ethers.utils.parseEther(`${price / 10 ** 18}`), gasLimit: 3000000}
     try {
         // show notification that tx is happening
-        const tx = await contract.buyItem(address, cardId, { value: price, gasLimit: 3000000 });
+        const tx = await contract.buyItem(address, cardId, options);
         const res = await tx.wait();
         console.log(res)
         // show notification that tx succesfull
